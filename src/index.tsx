5f482bc46d8046be3cc2f844c1ae0dadffa04355
shami <shamil79797@gmail.com>
2026-01-20 01:18:31 +0300

    Внесены новые компоненты MainScreen и CardInfo, а также обновлен корневой файл для отображения списка предложений.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainScreen from './pages/main/Main.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
+const listings = [
+  {
+    name: 'Beautiful & luxurious apartment at great location',
+    price: 120,
+    image: 'img/apartment-01.jpg',
+    type: 'Apartment' as const,
+    rating: 80,
+    isPremium: true,
+    bookmarked: false,
+  },
+  {
+    name: 'Wood and stone place',
+    price: 80,
+    image: 'img/room.jpg',
+    type: 'Room' as const,
+    rating: 80,
+    isPremium: false,
+    bookmarked: true,
+  },
+  {
+    name: 'Canal View Prinsengracht',
+    price: 132,
+    image: 'img/apartment-02.jpg',
+    type: 'Apartment' as const,
+    rating: 80,
+    isPremium: false,
+    bookmarked: false,
+  },
+  {
+    name: 'Nice, cozy, warm big bed apartment',
+    price: 180,
+    image: 'img/apartment-03.jpg',
+    type: 'Apartment' as const,
+    rating: 100,
+    isPremium: true,
+    bookmarked: false,
+  },
+];
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <MainScreen listings={listings}></MainScreen>
   </React.StrictMode>
 );
